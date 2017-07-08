ef22077249c18cb9f9436a363d6440f23ea4f9fe
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-07-08 11:23:35 +0200

    Networking

--- app/src/main/java/pl/training/stacjait/MainActivity.kt
+++ app/src/main/java/pl/training/stacjait/MainActivity.kt
@@ -4,8 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
+
+    private val eventsUrl = "http://213.32.66.195:8080/events.json"
 
     private val events = listOf(
             "Python - szybki start",
@@ -20,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         val eventsList = events_list
         eventsList.layoutManager = LinearLayoutManager(this)
         eventsList.adapter = EventsListAdapter(events)
+
+        doAsync {
+            EventsProvider(eventsUrl).get()
+            uiThread {
+                toast("Request completed")
+            }
+        }
     }
 
 }
